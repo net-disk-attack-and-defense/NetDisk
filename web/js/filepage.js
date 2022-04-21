7fbe3decb5e69bb65824995e8e1e161b1493a1a4
PM24962 <2496288505@qq.com>
2022-04-21 22:15:13 +0800

    统一下载与删除按钮功能

--- web/js/filepage.js
+++ web/js/filepage.js
@@ -2,3 +2,11 @@ function popoutToggle() {
     const popout = document.getElementById("popout_upload");
     popout.classList.toggle('active')
 }
+
+function changeAction(x) {
+    if (x.getAttribute("class") === "bt_download"){
+        document.getElementById("DoOrDe").getAttributeNode("action").value="FileDownload";
+    }else {
+        document.getElementById("DoOrDe").getAttributeNode("action").value="FileDelete";
+    }
+}
